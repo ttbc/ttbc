4fce2a00246f54becedcc988f5410fce3ddcad92
ttbc <tc@deskwolf.com>
2016-11-11 15:23:32 +0100

    ...

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-jQuery(document).ready(function($) {
+jQuery(document).ready(function ($) {
+    //jullpage script
     $('#fullpage').fullpage({
         //Navigation
         //menu: '#menu-secondary',
@@ -13,4 +14,23 @@ jQuery(document).ready(function($) {
         //Design
 //        paddingTop: '5em',
     });
+
+    // 3d animation on icon
+    $(document).mousemove(function (event) {
+        var cx = Math.ceil(window.innerWidth / 2.0),
+        cy = Math.ceil(window.innerHeight / 2.0),
+        dx = event.pageX - cx,
+        dy = event.pageY - cy,
+        tiltx = (dy / cy),
+        tilty = -(dx / cx),
+        radius = Math.sqrt(Math.pow(tiltx, 2) + Math.pow(tilty, 2)),
+        degree = (radius * -45);
+
+        $('.flipper').css('transform', 'rotate3d(' + tiltx + ', ' + tilty + ', 0, ' + degree + 'deg)');
+    });
+
+    $(document).mouseout(function () {
+        $('.flipper').css('transform', 'none');
+    });
+
 });
